8dec8f0c2ab09e5ec7547e98e66cb8ba065366b5
Huiming Zhang <me@yxzhm.com>
2021-01-07 06:23:12 +0000

    add word count

--- src/main/java/com/spark/poc/quick/start/SimpleApp.java
+++ src/main/java/com/spark/poc/quick/start/SimpleApp.java
@@ -1,11 +1,19 @@
 package com.spark.poc.quick.start;
 
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
 
 import com.spark.poc.Poc;
 
 import lombok.extern.slf4j.Slf4j;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
 
 @Slf4j
 public class SimpleApp extends Poc {
@@ -22,5 +30,12 @@ public class SimpleApp extends Poc {
         long numBs = logData.filter(s -> s.contains("b")).count();
         long numCs = logData.filter(s -> s.contains("c")).count();
         log.info("Lines with a: " + numAs + ", lines with b: " + numBs + ", lines with c: " + numCs);
+
+        JavaRDD<String> data = spark.sparkContext().textFile(logFile,5).toJavaRDD();
+        JavaPairRDD<String, Integer> counts = data.flatMap(x -> Arrays.asList(x.split(" ")).iterator())
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey(Integer::sum);
+        counts.saveAsTextFile("/opt/spark-data/word_count.txt");
+
     }
 }
